f5cc80856a5e121dc663ce38c3e017cfcf118e27
EduardoAlvesNeto <edualvezneto@gmail.com>
2023-04-13 15:14:52 -0300

    ✨ feat(eslint): Add support for Typescript

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,19 +1,38 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true,
+    'env': {
+        'es2021': true,
+        'node': true
     },
-    extends: 'eslint:recommended',
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
     },
-    rules: {
-        indent: ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-        'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
-    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
 };
