ed9714613f8bf8b36e73d1ac58923764411927c2
EduardoAlvesNeto <edualvezneto@gmail.com>
2023-06-15 16:30:14 -0300

    🧹 chore: simplifying the file

--- api/src/app/config/dotenv.ts
+++ api/src/app/config/dotenv.ts
@@ -1,21 +1,13 @@
 import 'dotenv/config';
 
 interface Config {
-  appPort: number,
-  dbUser: string,
-  dbPassword: string,
-  dbPort: number,
-  dbHost: string,
-  dbName: string
+  port: number,
+  secret: string
 }
 
 const config: Config = {
-    appPort: parseInt(process.env.PORT ?? '3000', 10),
-    dbUser: process.env.DATABASE_USER ?? '',
-    dbPassword: process.env.DATABASE_PASSWORD ?? '',
-    dbPort: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
-    dbHost: process.env.DATABASE_HOST ?? 'localhost',
-    dbName: process.env.DATABASE_NAME ?? '',
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  secret: process.env.JWT_SECRET ?? 'senhascreta'
 };
 
 export default config;
